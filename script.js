1654555b26480cece637c26845762084e949105e
danielpavlov <ge84nun@mytum.de>
2023-05-04 21:48:16 +0200

    aaaa

--- script.js
+++ script.js
@@ -1,27 +1,93 @@
 let idx = 0; // the current index of the url array
-let urls = [
-  "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/001/index.html",
-  "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/002/index.html",
-  "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/003/index.html",
-  "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/004/index.html",
-  "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/005/index.html",
-  "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/005b/index.html",
-  "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/006/index.html",
-  "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/007_2016/index.html",
-  "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/008/index.html",
-  "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/009/index.html",
-  "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/010/index.html",
-  "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/011/index.html",
-  "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/012/index.html",
-  "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/013/index.html",
-  "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/014/index.html",
-  "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/015/index.html",
-  "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/016/index.html",
-  "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/017/index.html",
-  "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/018/index.html",
-  "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/019/index.html",
-  "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/020/index.html",
-  "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/021/index.html",
+const urls = [
+  [
+    "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/001/index.html",
+    "Leber, Mensch - HE",
+  ],
+  [
+    "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/002/index.html",
+    "Cornea - HE",
+  ],
+  [
+    "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/003/index.html",
+    "Lippe, Mensch - HE",
+  ],
+  [
+    "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/004/index.html",
+    "Fingerbeere, Mensch - HE",
+  ],
+  [
+    "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/005/index.html",
+    "Nierenpapille - Azan",
+  ],
+  [
+    "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/005b/index.html",
+    "Nierenpapille - Azan",
+  ],
+  [
+    "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/006/index.html",
+    "Harnblase, Schwein - HE",
+  ],
+  [
+    "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/007_2016/index.html",
+    "Koronararterie Mensch - HE",
+  ],
+  [
+    "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/008/index.html",
+    "Colon, Mensch - Methylenblau-Fuchsin",
+  ],
+  [
+    "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/009/index.html",
+    "Pankreas, Mensch - Methylenblau-Fuchsin",
+  ],
+  [
+    "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/010/index.html",
+    "Mamma lactans, Ratte - HE",
+  ],
+  [
+    "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/011/index.html",
+    "Nabelschnur, Mensch HE",
+  ],
+  [
+    "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/012/index.html",
+    "Mamma nonlactans, Mensch - HE",
+  ],
+  [
+    "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/013/index.html",
+    "Braunes Fettgewebe, Ratte - HE",
+  ],
+  [
+    "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/014/index.html",
+    "Leber, retikuläre Fasern - Achucarro",
+  ],
+  [
+    "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/015/index.html",
+    "Milz, gespült, Schwein - Methylenblau-Eosin n. Mann",
+  ],
+  [
+    "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/016/index.html",
+    "Sehne, Mensch - Eisenhtx. Nach Heidenhain",
+  ],
+  [
+    "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/017/index.html",
+    "Elastisches Band, Rind - Methylenblau-Eosin",
+  ],
+  [
+    "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/018/index.html",
+    "Hyaliner Knorpel, Mensch - Hämalaun",
+  ],
+  [
+    "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/019/index.html",
+    "Elast. Knorpel, Ohr-Mensch neugeb. Res. Fuchs./v.G.",
+  ],
+  [
+    "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/020/index.html",
+    "Faserknorpel, Bandscheibe - Hämalaun",
+  ],
+  [
+    "https://virtuelle.anatomie.uni-marburg.de/vm/kurskasten/021/index.html",
+    "Scheitelbein, Mensch foet. - HE",
+  ],
 ];
 
 randomizeUrls();
@@ -33,8 +99,7 @@ function openNewTab() {
     idx++;
   }
 
-  let nextUrl = urls[idx];
-  console.log(nextUrl);
+  let nextUrl = urls[idx][0];
   window.open(nextUrl);
 }
 
@@ -43,4 +108,8 @@ function randomizeUrls() {
   urls.sort(function () {
     return 0.5 - Math.random();
   });
+}
+
+function showSolution() {
+  document.querySelector("h1").innerHTML = urls[idx][1];
 }
